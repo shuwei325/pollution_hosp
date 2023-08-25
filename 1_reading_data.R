cbe855670e061f6135ea6f92eeed0c0a8264e814
Shu Wei Chou-Chen <57328890+shuwei325@users.noreply.github.com>
2023-08-24 21:15:28 -0600

    merge aerosol data

--- 1_reading_data.R
+++ 1_reading_data.R
@@ -66,12 +66,59 @@ load(file="./Data/CHIRTS_CIGEFI_week_Region.RData")
 
 # Precipitación -----------------------------------------------------------
 
+load(file='./Data/datosPrecCHIRPS.RData')
+
+head(dat)
+range(dat$date)
+
+unique(dat$date)
 
 
-# AOD ---------------------------------------------------------------------
+datos_space <- dat %>% filter(date==dat$date[1]) %>% select(lon,lat)
+
+puntos <- st_as_sf(datos_space,remove=FALSE, coords =c("lon","lat"), 
+                   crs = 4326, agr = "constant")
 
 
+puntos_sf_joined <- st_join(puntos, union_sf)
 
+datos_space_joined <- as.data.frame(puntos_sf_joined) %>% select(lon, lat, Región)
+
+dat1 <- dat %>% 
+  left_join(datos_space_joined, by=c("lon","lat")) %>% 
+  na.omit()
+
+datos_CHIRPS <- dat1 %>% 
+            mutate(year = year(date),
+                   week = epiweek(date)) %>%
+            group_by(year,week,Región) %>% summarise(precip = sum(chirps)) %>%
+  ungroup()
+datos_CHIRPS <- datos_CHIRPS %>%  mutate(date=aweek::get_date(week=week,year=year))
+
+
+# AOD D3 ---------------------------------------------------------------------
+
+
+dirbase <- "./Data/MOD08_D3/"
+listfilesg <- list.files(path = dirbase,pattern = paste0("*.RData"), recursive = TRUE)
+
+aerosol<-numeric()
+for (i in 1:length(listfilesg)) {
+  load(file = paste0(dirbase, listfilesg[i]))
+  aerosol<- rbind(aerosol,datos_aerosol)
+}       
+
+head(aerosol)
+
+#un error en la descarga de datos
+names(aerosol)<- c("lat", "lon", "aerosol", "year", "day", "date")
+
+datos_aerosol <- aerosol %>% 
+  mutate(week = epiweek(date)) %>%
+  group_by(year,week) %>% summarise(aerosol = mean(aerosol,na.rm=TRUE)) %>%
+  ungroup()
+
+datos_aerosol <- datos_aerosol %>% mutate(date=aweek::get_date(week=week,year=year))
 
 # Joint data --------------------------------------------------------------
 
@@ -82,7 +129,16 @@ datos_finales <- egresos_region_semana %>% filter(year>=2000 & year<=2019) %>%
                             left_join(datos_CHIRTS, by=c("Región"="Región",
                                                          "date"="date",
                                                          "year"="year",
-                                                         "epi.week"="week"))
+                                                         "epi.week"="week")) %>%
+                            left_join(datos_CHIRPS, by=c("Región"="Región",
+                                                         "date"="date",
+                                                         "year"="year",
+                                                         "epi.week"="week"))  %>%
+                            left_join(datos_aerosol, by=c("date"="date",
+                                                          "year"="year",
+                                                          "epi.week"="week")) 
+
+
 
 
 save(datos_finales,file="datos_finales.RData")
--- 2_descriptive_analysis_region.R
+++ 2_descriptive_analysis_region.R
@@ -7,9 +7,21 @@ library(tidyverse)
 library(latticeExtra)
 library(zoo)
 library(RColorBrewer)
-
+library(car)
+library(astsa)
 
 load(file="datos_finales.RData")
+
+
+datos_finales <- datos_finales %>% mutate(log_precip=log(precip+1),
+                                          egreso_1 = lag(egreso),
+                                          egreso_2 = lag(egreso,n=2),
+                                          dif_egreso=egreso-lag(egreso)) %>% na.omit()
+
+hist(datos_finales$log_precip)
+
+# Egresos -----------------------------------------------------------------
+
 
 #Egresos nacionales
 
@@ -27,4 +39,105 @@ plot_CR2 <- datos_finales %>%
   geom_point()
 
 plot_CR2
+
+
+# Clima -----------------------------------------------------------------
+
+#Tmax por región
+datos_finales %>% 
+  ggplot( aes(x=date, y=Tmax, group = Región, color = Región)) +
+  geom_line() +
+  geom_point()
+
+#Tmin por región
+datos_finales %>% 
+  ggplot( aes(x=date, y=Tmin, group = Región, color = Región)) +
+  geom_line() +
+  geom_point()
+
+#Precip por región
+datos_finales %>% 
+  ggplot( aes(x=date, y=precip, group = Región, color = Región)) +
+  geom_line() +
+  geom_point()
+
+datos_finales %>% 
+  ggplot( aes(x=date, y=log_precip, group = Región, color = Región)) +
+  geom_line() +
+  geom_point()
+
+#AOD
+datos_finales %>% filter(Región=="Brunca")%>%
+  ggplot( aes(x=date, y=aerosol, group = Región, color = Región)) +
+  geom_line() +
+  geom_point()
+
+
+
+
+# correlacion -------------------------------------------------------------
+
+names(datos_finales)
+corr <- round(cor(datos_finales[,-c(1,2,3,5)]), 4)
+
+ggcorrplot(corr, type = "lower",
+           lab = TRUE,lab_size = 3)
+
+with(datos_finales,plot(egreso~log_precip))
+
+scatterplotMatrix(~egreso+Tmax+Tmin+precip,data=datos_finales)
+
+
+
+# Por región --------------------------------------------------------------
+
+Region_B <- datos_finales %>% filter(Región == "Brunca")
+Region_CN <- datos_finales %>% filter(Región == "Central Norte")
+Region_CS <- datos_finales %>% filter(Región == "Central Sur")
+Region_CH <- datos_finales %>% filter(Región == "Chorotega")
+Region_HA <- datos_finales %>% filter(Región == "Huetar Atlántica")
+Region_HN <- datos_finales %>% filter(Región == "Huetar Norte")
+Region_PC <- datos_finales %>% filter(Región == "Pacífico Central")
+
+Region<-Region_CN
+
+with(Region,plot(egreso,Tmax))
+with(Region,plot(egreso,Tmin))
+with(Region,plot(egreso,log_precip))
+with(Region,plot(egreso,aerosol))
+
+(corr<-round(cor(Region[,c(4,6,7,8)]), 4))
+plot_corr <- ggcorrplot(corr, type = "lower",
+                        lab = TRUE,lab_size = 3)
+
+acf(Region$egreso,lag.max=100)
+ccf(Region$egreso,Region$Tmax,lag.max=40)
+ccf(Region$egreso,Region$Tmin,lag.max=40)
+ccf(Region$egreso,Region$log_precip,lag.max=40)
+ccf(Region$egreso,Region$aerosol,lag.max=40)
+
+with(Region,lag2.plot (Tmax,egreso, 10))
+with(Region,lag2.plot (Tmin,egreso, 10))
+with(Region,lag2.plot (log_precip,egreso, 10))
+with(Region,lag2.plot (aerosol,egreso, 10))
+
+#modelos lineales
+
+mod0<-lm(egreso~Tmax+Tmin+precip+aerosol,data=Region)
+summary(mod0)
+mod1<-lm(egreso~egreso_1+Tmax+Tmin+precip+aerosol,data=Region)
+summary(mod1)
+mod2<-lm(egreso~egreso_1+egreso_2+Tmax+Tmin+precip+aerosol,data=Region)
+summary(mod2)
+anova(mod1,mod2)
+
+plot(Region$egreso,type="l")
+points(fitted(mod2),type="l",col=2)
+
+plot(Region$egreso,fitted(mod0))
+
+
+
+
+
 
